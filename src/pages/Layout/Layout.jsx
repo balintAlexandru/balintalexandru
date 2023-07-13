715900cbc0da15098ee228e5e0eac812d71a768d
Balint Alexandru <balintalex737@gmail.com>
2023-07-13 15:40:19 +0300

    add mobile package

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,16 +1,19 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import "./Layout.scss";
+import Div100vh from "react-div-100vh";
 
 const Layout = ({ children, setTriggerContactAnimation }) => {
   return (
-    <div className="layout-container">
-      <Header />
-      <main>
-        <div>{children}</div>
-      </main>
-      <Footer setTriggerContactAnimation={setTriggerContactAnimation} />
-    </div>
+    <Div100vh>
+      <div className="layout-container">
+        <Header />
+        <main>
+          <div>{children}</div>
+        </main>
+        <Footer setTriggerContactAnimation={setTriggerContactAnimation} />
+      </div>
+    </Div100vh>
   );
 };
 
